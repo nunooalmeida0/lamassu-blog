d7566816c87f4c0a131987d1892c3e73a37fceb2
nunooalmeida0 <hello@nunooalmeida.com>
2020-01-14 15:45:23 +0000

    menu alignment and border bottom

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Img from "gatsby-image"
+
 
 const Header = ({ siteTitle }) => (
-<header leftComponent = {{ icon: ''}}>
-        <div className="menu-wrapper">
-          <Img className="logo menu-item" src="images/logo.svg" alt="Lamassu logo"/>
-          <ul className="header-list">
-            <li className="menu-item">
-              <a href="https://lamassu.is/products" title="Products">Products</a>
-            </li>
-            <li className="menu-item">
-              <Link href="https://lamassu.is/how-to-start">How to Start</Link>
-            </li>
-            <li className="menu-item"> 
-              <Link href="https://lamassu.is/faq">Q&A</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="https://lamassu.is/sla-support">SLA</Link>
-            </li>
-            <li className="menu-item">
-              <Link href="localhost:8000">Blog</Link>
-            </li>          
-            <li className="menu-item">
-              <Link href="https://lamassu.is/Contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
+  <header>
+    <div className="menu-wrapper">
+      <a className="menu-item" href="https://lamassu.is">
+        <img className="logo" src={'../../logo.svg'} alt="Lamassu logo"/> 
+      </a>
+      
+      <ul className="header-list">
+        <li className="menu-item">
+          <a href="https://lamassu.is/products" title="Products">Products</a>
+        </li>
+        <li className="menu-item">
+          <a href="https://lamassu.is/how-to-start">How to Start</a>
+        </li>
+        <li className="menu-item"> 
+          <a href="https://lamassu.is/faq">Q&A</a>
+        </li>
+        <li className="menu-item">
+          <a href="https://lamassu.is/sla-support">SLA</a>
+        </li>
+        <li className="menu-item">
+          <a className="selected" href="/">Blog</a>
+        </li>          
+        <li className="menu-item">
+          <a href="https://lamassu.is/products">Contact</a>
+        </li>
+      </ul>
+    </div>
 
-      </header>
+    </header>
   //   <div
   //     style={{
   //       margin: `0 auto`,
